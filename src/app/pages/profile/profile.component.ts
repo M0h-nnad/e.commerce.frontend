e5a0751df676c3461a174a0177468238b4cb51ab
unknown <kingk4170@gmail.com>
2023-02-12 10:49:07 +0000

    profile

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { ErrorHandler } from 'src/app/services/error.handler';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
+  profileFormGroup!: FormGroup;
+  errors: any = {};
+  id!: string;
+  constructor(
+    private readonly fb: FormBuilder,
+    private readonly toastr: ToastrService,
+    private readonly userService: UserService,
+    private readonly errorHandler: ErrorHandler
+  ) {}
 
   ngOnInit(): void {
+    this.initForm();
+    this.errorHandler.handleErrors(this.profileFormGroup, this.errors);
   }
 
+  initForm() {
+    this.profileFormGroup = this.fb.group({
+      firstName: ['', [Validators.required, Validators.maxLength(50)]],
+      lastName: ['', [Validators.required, Validators.maxLength(50)]],
+      phone: ['', [Validators.required, Validators.maxLength(50)]],
+      email: [
+        '',
+        [Validators.required, Validators.maxLength(50), Validators.email],
+      ],
+      gender: ['', [Validators.required, Validators.maxLength(50)]],
+    });
+  }
+
+  save() {
+    this.userService.updateUser(this.profileFormGroup.value).subscribe({
+      next: (res: any) => this.toastr.success(res.messages),
+      error: (err) => this.toastr.error(err.error.messages),
+    });
+  }
 }
---
+++ src/app/services/user/user.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, first, throwError } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  constructor(private http: HttpClient) {}
+
+  updateUser(body: any) {
+    return this.http
+      .put(`${environment.mainUrl}${environment.updateUser}`, body)
+      .pipe(
+        first(),
+        catchError((e) => throwError(() => e))
+      );
+  }
+}
